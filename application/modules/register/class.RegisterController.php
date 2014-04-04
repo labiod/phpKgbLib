66d400ed87cbb298747e067e77c0637c7e75a54e
Gabunia <g@gmail.com>
2014-04-04 18:34:14 +0200

    poprawki wyglądowe + captcha

--- application/modules/register/class.RegisterController.php
+++ application/modules/register/class.RegisterController.php
@@ -11,6 +11,7 @@ class RegisterController extends BasicIndexController {
 		$this->_view->title = "Lpunkt.pl - Rejestracja";
 	}
 	function registerAction() {
+                session_start();
 		$this->_view->title = "Lpunkt.pl - Rejestracja";
 		$tab = $this->getParametersMap ();
 		if (isset ( $tab ['submit'] )) {
@@ -32,6 +33,16 @@ class RegisterController extends BasicIndexController {
 				unset ( $tab ['role'] );
 				$tmp = $role->current ();
 				$tab ["role_id"] = $tmp ["id_role"];
+                                //captcha
+                                include_once $_SERVER['DOCUMENT_ROOT'] . '/securimage/securimage.php';
+                        	$securimage = new Securimage();
+                                if ($securimage->check($_POST['captcha_code']) == false) {
+                                    // the code was incorrect
+                                    $this->setMessage("Podany kod jest niezgodny! Spróbuj ponownie.");
+                                        $link = "Location: /register";
+                                        header ( $link ); return;
+                                }
+                                
 				$index = $this->_users->insert ( $tab );
 				
 				$this->setMessage ( "Rejestracja powiodła się! Zaloguj się!" );
--- application/modules/register/view/index.php
+++ application/modules/register/view/index.php
@@ -6,16 +6,21 @@
         <legend id="step_1">Krok 1</legend><legend id="step_2">Krok 2</legend><legend id="step_3">Krok 3</legend>
         <div class="clear"></div>
     <div id="stepdiv_1">
+        <label for="imie">Imię</label> 
+        <input name="imie" id="imie" type="text" required /> <br /> 
+        <label for="nazwisko">Nazwisko</label>
+        <input name="nazwisko" id="nazwisko" type="text" required /> <br /> 
+    </div>    
+    <div id="stepdiv_2">
         <label for="email_adr">em@il</label> 
         <input name="email" id="email_adr" type="email" required /> <br /> 
         <label for="pass">Hasło</label>
         <input type="password" name="password" id="pass" required /> <br /> 
-    </div>    
-    <div id="stepdiv_2">
-        Miałeś mi powiedzieć, co tu ma być...
     </div>   
     <div id="stepdiv_3">
-       I tu też!
+       <img id="captcha" src="/public/securimage/securimage_show.php" alt="CAPTCHA Image" />
+       <input type="text" name="captcha_code" size="10" maxlength="6" />
+       <a href="#" onclick="document.getElementById('captcha').src = '/public/securimage/securimage_show.php?' + Math.random(); return false">[ Different Image ]</a>
     </div> 
     <input type="button" value="Wstecz" id="wstecz" class="blue_button_big" hidden/>
     <input type="button" value="Dalej" id="dalej" class="blue_button_big" />
