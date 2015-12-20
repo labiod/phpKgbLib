ba936b93eaa648d66578e219078ddbaf8c7e271d
gabunia <gabriela.betlej@gmail.com>
2015-12-20 14:20:38 +0100

    update submodule

---
+++ cache/1431793836_config.ini.php
@@ -0,0 +1,20 @@
+<?php
+ $table['db.host'] = 'localhost';
+$table['db.dbname'] = 'lpunkt';
+$table['db.user'] = 'root';
+$table['db.password'] = '';
+$table['db.charset'] = 'utf8';
+$table['icon.add'] = '/public/images/panel/add.png';
+$table['icon.edit'] = '/public/images/panel/modify_m.png';
+$table['icon.delete'] = '/public/images/panel/delete_m.png';
+$table['icon.view'] = '/public/images/panel/info_m.png';
+$table['icon.logout'] = '/public/images/panel/exit_m.png';
+$table['icon.accept'] = '/public/images/panel/accept_icon.png';
+$table['user.logout'] = 'allow';
+$table['user.login'] = 'allow';
+$table['index.index'] = 'allow';
+$table['register.index'] = 'allow';
+$table['build.type'] = 'debug';
+$table['build.version'] = '0.1.1';
+$table['build.updateDate'] = '12-04-2015';
+?>
